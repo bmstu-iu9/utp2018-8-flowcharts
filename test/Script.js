92d8140b0ce7f29289d8e4b6f6d2e2dc538a2ab2
Alexander <brfh-1999@bk.ru>
2018-07-17 21:32:29 +0300

    выделение облости+

--- test/Script.js
+++ test/Script.js
@@ -11,47 +11,6 @@ document.addEventListener("drop", function(event) {
     if ( event.target.className == "droptarget" ) {
         var data = event.dataTransfer.getData("Text");
         event.target.appendChild(document.getElementById(data));
+        event.target.className = "lv";
     }
 });
-
-
-
-
-
-
-
-function removeClassName (elem, className) {
-	elem.className = elem.className.replace(className, "").trim();
-}
-
-function addCSSClass (elem, className) {
-	removeClassName (elem, className);
-	elem.className = (elem.className + " " + className).trim();
-}
-
-String.prototype.trim = function() {
-	return this.replace( /^\s+|\s+$/, "" );
-}
-
-function stripedTable() {
-	if (document.getElementById && document.getElementsByTagName) {  
-		var allTables = document.getElementsByTagName('table');
-		if (!allTables) { return; }
-
-		for (var i = 0; i < allTables.length; i++) {
-			if (allTables[i].className.match(/[\w\s ]*scrollTable[\w\s ]*/)) {
-				var trs = allTables[i].getElementsByTagName("tr");
-				for (var j = 0; j < trs.length; j++) {
-					removeClassName(trs[j], 'alternateRow');
-					addCSSClass(trs[j], 'normalRow');
-				}
-				for (var k = 0; k < trs.length; k += 2) {
-					removeClassName(trs[k], 'normalRow');
-					addCSSClass(trs[k], 'alternateRow');
-				}
-			}
-		}
-	}
-}
-
-window.onload = function() { stripedTable(); }
