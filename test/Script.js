de3213383ede2ebbe633cb9f815e45cbceb21aed
Alexander <brfh-1999@bk.ru>
2018-07-19 23:51:23 +0300

    добавлено расширение по столбцам

--- test/Script.js
+++ test/Script.js
@@ -1,3 +1,6 @@
+var columns = [false,true,false];
+var mainColumn=1;
+
 document.addEventListener("dragstart", function(event) {
     event.dataTransfer.setData("Text", event.target.id);
 });
@@ -5,7 +8,7 @@ document.addEventListener("dragstart", function(event) {
 document.addEventListener("dragover", function(event) {
     event.preventDefault();
 });
-
+ 
 document.addEventListener("drop", function(event) {
     event.preventDefault();
     if ( event.target.className == "droptarget" ) {
@@ -21,7 +24,7 @@ document.addEventListener("drop", function(event) {
     	}	else {
     		event.target.className = "lc";
     	}
-        startNode.appendChild(start);
+    	startNode.appendChild(start);
         changeTrigger(row, cell,data.id);
     }
 });
@@ -29,8 +32,64 @@ document.addEventListener("drop", function(event) {
 
 function changeTrigger(row, cell, type){
 	var table = document.getElementById("workSpace");
-	table.rows[row+1].cells[cell].className= "droptarget"; 
+	if (type !== "end"){
+			table.rows[row+1].cells[cell].className= "droptarget"; 
+	}
 	if (type=== "romb"){ 
-		table.rows[row].cells[cell+1].className= "droptarget";
+		var newColumn = findFreeColumn(cell)
+		table.rows[row].cells[newColumn].className= "droptarget";
+	}
+}
+
+function findFreeColumn(startColumn){
+	var table = document.getElementById("workSpace");
+	if (startColumn<mainColumn || (!columns[mainColumn-1] && mainColumn== startColumn)){
+		for (var i=startColumn-1;i>=0;i--){
+			if (!columns[i]){
+				if(i==0){
+					addColumn(true);
+					i++;
+				}
+				columns[i]=true;
+				return i ;
+			}
+		}
+	} else {
+		alert();
+		for (i=startColumn+1;i<columns.length;i++){
+			if (!columns[i]){
+				if(i==columns.length-1){
+					addColumn(false);
+				}
+				columns[i]=true;
+				return i ;
+			}
+		}
+	}
+} 
+
+function addColumn(side){
+	var newCellClass;
+	var table = document.getElementById("workSpace");
+	var pos =0;
+	if (!side){
+		pos=columns.length-1;
+	}
+	if (table.rows[0].cells[pos].className=== "lv"){
+		newCellClass ="lc";
+	} else {
+		newCellClass ="lv";
+	}
+	for (var i =0;i<table.rows.length;i++){
+		var newCell= document.createElement("td");
+		newCell.className=newCellClass;
+		if (!side){
+			table.rows[i].appendChild(newCell);
+			columns.push(false);
+		} else{
+			table.rows[i].insertBefore(newCell,table.rows[i].children[0]);
+			columns.unshift(false);
+			mainColumn++;
+		}
 	}
 }
