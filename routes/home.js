9d29e7bd2920bfc15f38568f6da9dadd53e1f7e7
kir_snegir <ssnegurkirill@gmail.com>
2018-09-09 17:50:19 +0300

    Теперь индекс хтмл работает без сбоев

--- routes/home.js
+++ routes/home.js
@@ -5,7 +5,7 @@ function home (req, res) {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
 
-    const stream = fs.createReadStream(path.resolve('index.html'));
+    const stream = fs.createReadStream(path.resolve('WorkZone/index.html'));
 
     stream.pipe(res);
 }
