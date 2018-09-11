5397d4d350ab557f20491d641bd5de4184c54756
Alexander <brfh-1999@bk.ru>
2018-09-12 00:57:50 +0300

    dd

--- WorkZone/Script.js
+++ WorkZone/Script.js
@@ -827,6 +827,11 @@ function buttonPlay(){
     infinitLoop=0;
     var V =graph[0];
     while(V.type!="end"){
+        if (infinitLoop>100){
+            alert("error of loop");
+            buttonReStart();
+            return;
+        }
         if (V.dead){
             alert("any block is already dead");
             return;
@@ -876,11 +881,6 @@ function buttonPlay(){
             break;
         }
         V=graph[V.childs[0]];
-        if (infinitLoop>100){
-            alert("error of loop");
-            buttonReStart();
-            return;
-        }
     }
     setRes();
     reSetM();
